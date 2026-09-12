2b4922e2005866331ed065ad7c530c68b663f69e
Hardhat Chad <hardhatchad@gmail.com>
2026-09-12 13:18:19 -0700

    security.txt

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -3,8 +3,9 @@ mod unwrap;
 
 use bury::*;
 use grams_api::instruction::*;
-use unwrap::*;
+use solana_security_txt::security_txt;
 use steel::*;
+use unwrap::*;
 
 pub fn process_instruction(
     program_id: &Pubkey,
@@ -22,3 +23,12 @@ pub fn process_instruction(
 }
 
 entrypoint!(process_instruction);
+
+security_txt! {
+    name: "Grams",
+    project_url: "https://ore.supply",
+    contacts: "email:hardhatchad@gmail.com,discord:hardhatchad",
+    policy: "https://github.com/regolith-labs/grams/blob/master/SECURITY.md",
+    preferred_languages: "en",
+    source_code: "https://github.com/regolith-labs/grams"
+}
